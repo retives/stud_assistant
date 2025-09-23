febb21c56a8e96d87f33921546cebfedb0b46e6c
retives <srgtkrvvv@proton.me>
2025-09-23 23:50:11 +0300

    Configure db

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,10 +1,10 @@
-from sqlalchemy import SQLAlchemy
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import text
+SQLALCHEMY_DATABASE_URL = 'postgresql://stud_user:student@localhost:5432/stud_assistant'
 
-SQL_ALCHEMY_DATABASE_URL = 'postgresql://stud_user:student@localhost:5432/stud_assistant'
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
@@ -16,3 +16,25 @@ def get_db():
         yield db
     finally:
          db.close()
+
+
+def create_database():
+
+    """
+    Run this only when creating the database!!!
+    """
+    Base.metadata.create_all(bind=engine)
+
+def test_connection():
+    try:
+        # create a session
+        db: Session = SessionLocal()
+        # run a simple query (count users)
+        result = db.execute(text("CREATE TABLE temp(name VARCHAR(255));"))
+        print("Database connection successful:", result.scalar())
+    except Exception as e:
+        print("Database connection failed:", e)
+    finally:
+        db.close()
+if __name__ == '__main__':
+    create_database()
