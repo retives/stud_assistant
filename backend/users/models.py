338e1526f1fa2b74b5d24499860d108c66d35341
retives <srgtkrvvv@proton.me>
2025-09-23 13:10:24 +0300

    Deleted django: switching to fast API

--- backend/users/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-# Create your models here.
-
-class User(AbstractUser):
-    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
