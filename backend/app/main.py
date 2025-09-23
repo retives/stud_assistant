e83fb9f4427e42e923ce766aa6553b4971b36428
retives <srgtkrvvv@proton.me>
2025-09-23 23:49:46 +0300

    Sinup view

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from app.api.endpoints import signup
+
 
 app = FastAPI()
+app.include_router(signup.router) 
+
+
 
 @app.get('/')
 async def root():
     return {"message":"hello"}
+
