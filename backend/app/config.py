7de2d4e39dbfd52c0c139ebefcd4e953d1e5e6f1
Serhii Tokariev <tr3e1t0ry@gmail.com>
2025-10-22 10:48:55 +0300

    Create system id

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,7 +1,9 @@
 from dotenv import load_dotenv
+from uuid import UUID
 # JWT configuration
 
 # in seconds
 TOKEN_EXPIRE_TIME = 3600
 SECRET_KEY = str(load_dotenv('SECRET_KEY'))
 ALGORITHM = 'HS256'
+SYSTEM_ID = UUID("00000000-0000-0000-0000-000000000001")
