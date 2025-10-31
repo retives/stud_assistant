919a79b809bf8d5ba8a6d32f1801b2392f4dd196
retives <srgtkrvvv@proton.me>
2025-10-31 16:03:09 +0200

    Add the table refresh

--- backend/app/database.py
+++ backend/app/database.py
@@ -31,4 +31,12 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 if __name__ == '__main__':
-    create_tables()
+    print(Base.metadata.tables.keys())
+    from app.models import Base, Message
+    print(Base.metadata.tables.keys())
+    print(Message.__table__.columns.keys())
+    import app.models
+    print(app.models.__file__)
+
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
