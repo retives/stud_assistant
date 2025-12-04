ea60ba50e6a6ba33d3c15f5009af7e32ad9b96c0
srgtkrvvv <tr3e1t0ry@gmail.com>
2025-12-04 09:59:16 +0200

    Add route to frontend

--- frontend/stud_assistant/src/router/index.ts
+++ frontend/stud_assistant/src/router/index.ts
@@ -2,12 +2,15 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Chat from '@/components/Chat.vue'
 import Login from '@/components/Login.vue'
 import Signup from '@/components/Signup.vue'
+import Payment from '@/components/Payment.vue'
+
 
 const routes = [
   { path: '/', name: 'Home', component: Chat },
   { path: '/chat/:id', name: 'Chat', component: Chat, props: true },
   { path: '/login', name: 'Login', component: Login },
   { path: '/signup', name: 'Signup', component: Signup },
+  { path: '/subscription', name: 'Subscription', component: Payment },
 ]
 
 
